29ceb857778f91cb32ff09fdef15554708fa9297
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2017-06-17 12:11:01 +0000

    Create pages boilerplate for routing

---
+++ src/containers/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1>Home Page</h1>
+                <p>This is a template showcasing the optional theme stylesheet included in Bootstrap. Use it as a starting point to create something more unique by building on or modifying it.</p>
+            </div>
+        )
+    }
+}
+
+export default HomePage;
---
+++ src/containers/UpcomingPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class UpcomingPage extends Component {
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1>Upcoming Movies</h1>
+                <p>This is a template showcasing the optional theme stylesheet included in Bootstrap. Use it as a starting point to create something more unique by building on or modifying it.</p>
+            </div>
+        )
+    }
+}
+
+export default UpcomingPage;

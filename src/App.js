8aa936952260ae5fe7280869e05a2b32b3c9c276
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2017-06-17 12:12:01 +0000

    Configure react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
+
+import { Route } from 'react-router-dom';
+
+import Header from './components/blocks/Header';
+
+import HomePage from './containers/HomePage'
+import TopSeriesPage from './containers/TopSeriesPage'
+import UpcomingPage from './containers/UpcomingPage'
+
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <h1>Movies finder</h1>
+                <Header/>
+
+                <div className="container">
+                    <Route exact path="/" component={HomePage}/>
+                    <Route exact path="/upcoming" component={UpcomingPage}/>
+                    <Route exact path="/series" component={TopSeriesPage}/>
+                </div>
             </div>
         );
     }

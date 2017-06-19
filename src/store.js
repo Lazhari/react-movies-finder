c59467baf32567889df2cd861e880d9b5a03f96e
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2017-06-19 04:46:09 +0000

    Update the store

--- src/store.js
+++ src/store.js
@@ -1,3 +1,10 @@
-/**
- * Created by macbookproretina13 on 6/17/17.
- */
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducer from './reducers';
+
+const middleware = composeWithDevTools(applyMiddleware(promise(), thunk));
+
+export default createStore(rootReducer, middleware);

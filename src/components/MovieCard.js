28ed36126fbb98f2d37506923dc215c29d1b4d9b
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2017-06-19 05:27:28 +0000

    Trancate movie overview

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { truncate } from 'underscore.string';
+
 import './styles/MovieCard.css';
 import placeholderImage from '../Images/abstract-image.jpg';
 
@@ -14,12 +16,12 @@ const MovieCard = ({movie, loading}) => {
             <div className="card-block">
                 <h4 className="card-title mt-3">{movie.original_title}</h4>
                 <div className="card-text">
-                    {movie.overview}
+                    {truncate(movie.overview, 50)}
                 </div>
             </div>
             <div className="card-footer">
                 <small>{movie.release_date || 'Unknown Released Date'}</small>
-                <strong className="pull-right text-danger">{movie.vote_average} / 10</strong>
+                <strong className="pull-right text-danger">{movie.vote_average} <i className="glyphicon glyphicon-star"></i></strong>
             </div>
         </div>
     )

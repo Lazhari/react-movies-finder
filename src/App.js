1eb6a5f1a605b35eec3b9fa947d4c72b4fc999f0
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2017-06-19 04:46:40 +0000

    Update App component and index page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import React, {Component} from 'react';
 import { Route } from 'react-router-dom';
 
 import Header from './components/blocks/Header';
-import Sidebar from './components/blocks/Sidebar';
 
 import HomePage from './containers/HomePage';
 import TopSeriesPage from './containers/TopSeriesPage';
 import UpcomingPage from './containers/UpcomingPage';
-import GenrePage from './containers/GenrePage'
+import GenrePage from './containers/GenrePage';
+import MoviesCategoryContainer from './containers/MoviesCategoryContainer';
 
 import './App.css';
 
 class App extends Component {
+
     render() {
         return (
             <div>
@@ -21,13 +22,13 @@ class App extends Component {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-xs-12 col-sm-4 col-md-3 col-lg-2">
-                            <Sidebar/>
+                            <MoviesCategoryContainer/>
                         </div>
                         <div className="col-xs-12 col-sm-8 col-md-9 col-lg-10">
                             <Route exact path="/" component={HomePage}/>
                             <Route exact path="/upcoming" component={UpcomingPage}/>
                             <Route exact path="/series" component={TopSeriesPage}/>
-                            <Route exact path="/genre/:id/:genre" component={GenrePage}/>
+                            <Route exact path="/genres/:id/:genre" component={GenrePage}/>
                         </div>
                     </div>
                 </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './store';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(
     <Router>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </Router>,
     document.getElementById('root'));
 registerServiceWorker();

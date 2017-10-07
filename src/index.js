392d49e77154b2d3fd78f9254ba479c4b17a3971
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2017-10-07 22:28:15 +0100

    Fixing the routing issue on github pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './store';
@@ -10,7 +10,7 @@ import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
 ReactDOM.render(
-    <Router>
+    <Router basename="/">
         <Provider store={store}>
             <App />
         </Provider>

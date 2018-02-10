c6e5a3038b805eab0506c4ae701264fb36a13b4b
Lazhari <lazhari.mohammed@outlook.com>
2018-02-10 14:09:33 +0000

    Improve the Actor card ui

--- src/components/ActorCard.js
+++ src/components/ActorCard.js
@@ -9,16 +9,21 @@ const ActorCard = ({actor}) => {
     const onErrorLoadingImage = (e) => {
         e.target.src = placeholderImage;
     };
+    const imageStyle = {
+        display: 'block',
+        height: '40vh',
+        clipPath: 'polygon(0 0, 100% 0%, 100% 90%, 0 100%)'
+    };
     return (
 
         <Card>
             <CardImg top width="100%" src={`https://image.tmdb.org/t/p/w138_and_h175_bestv2${actor.profile_path}`}
-                     alt={actor.name} onError={onErrorLoadingImage}/>
+                     alt={actor.name} onError={onErrorLoadingImage} style={imageStyle}/>
             <CardBody>
-                <CardTitle className="h6">
+                <CardTitle className="h6 text-center" style={{fontSize: 14, height: 40}}>
                     <a href="#">Card {actor.name}</a>
                 </CardTitle>
-                <CardSubtitle>{actor.character}</CardSubtitle>
+                <CardSubtitle style={{fontSize: 12}} className="text-center">{actor.character}</CardSubtitle>
             </CardBody>
         </Card>
 

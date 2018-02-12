37f76c832294f5d0d12b0682ea43cde0ca986a45
Lazhari <lazhari.mohammed@outlook.com>
2018-02-12 01:42:56 +0000

    Update the movie card css and the theme colors

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -17,38 +17,26 @@ const MovieCard = ({movie, loading, hideOverview}) => {
     const imageStyle = {
         display: 'block',
         height: '40vh',
-        clipPath: 'polygon(0 0, 100% 0%, 100% 90%, 0 100%)',
+        //clipPath: 'polygon(0 0, 100% 0%, 100% 90%, 0 100%)',
         objectFit: 'cover',
+        borderRadius: 5.5,
         objectPosition: 'center'
     };
     const truncatedOverview = (<span>{truncate(movie.overview, 115)} <NavLink to={`/movies/${movie.id}`}>Read more</NavLink></span>);
     return (
-        <Card>
+        <Card style={{backgroundColor: 'rgba(0,0,0,0)'}}>
             <CardImg top width="100%" src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}`}
                      alt={movie.original_title} onError={onErrorLoadingImage} style={imageStyle}/>
-            <CardBody>
-                <CardTitle className="text-black text-center h6" style={{height: 40}}>
-                    <NavLink to={`/movies/${movie.id}`} style={{color: '#494c62', textDecoration: 'none'}}>
+            <CardBody style={{padding: 10}}>
+                <CardTitle className="" style={{height: 34, color: '#e9e9e9',fontWeight: 'bold', textAlign: 'left', fontSize: 16}}>
+                    <NavLink to={`/movies/${movie.id}`} style={{textDecoration: 'none'}}>
                         {movie.original_title}
                     </NavLink>
                 </CardTitle>
-                <CardText className="card-text text-justify" style={{fontSize: 12,height: 55}}>
-                    {
-                        (movie.overview && movie.overview.length > 115) ?
-                            truncatedOverview :
-                            movie.overview
-                    }
-                </CardText>
-            </CardBody>
-            <CardText className="card-footer bg-white">
-                <span className="text-muted">
-                    {movie.vote_average} <i className="fa fa-star text-warning"></i>
+                <span style={{color: '#ffffff', opacity: 0.8, fontSize: 14}}>
+                        {getYear(movie.release_date) || 'Unknown Released Date'}
                 </span>
-                <span className="text-muted" style={{float: 'right'}}>
-                        <span
-                            className="badge badge-primary">{getYear(movie.release_date) || 'Unknown Released Date'}</span>
-                    </span>
-            </CardText>
+            </CardBody>
         </Card>
     )
 };

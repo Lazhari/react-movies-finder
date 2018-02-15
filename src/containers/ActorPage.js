1830e1eb328376b69eae5a6e9d3f8ee9acd3563b
Lazhari <lazhari.mohammed@outlook.com>
2018-02-15 23:51:11 +0000

    Create the actor page

---
+++ src/containers/ActorPage.js
@@ -0,0 +1,13 @@
+import react , {Component} from 'react';
+
+class ActorPage extends Component {
+    render() {
+        return (
+            <div>
+                Actor Page
+            </div>
+        )
+    }
+}
+
+export default ActorPage;

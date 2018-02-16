ca205f531a938687dca661812151c898eb2b7028
Lazhari <lazhari.mohammed@outlook.com>
2018-02-16 00:32:46 +0000

    Get the actor profile

--- src/containers/ActorPage.js
+++ src/containers/ActorPage.js
@@ -1,21 +1,68 @@
 import React , {Component} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+
+import {Card, CardImg, CardTitle, CardSubtitle, CardBody} from 'reactstrap';
+import {
+    getPeopleProfile
+} from '../actions/actorAction';
+
+import placeholderImage from '../Images/abstract-image.jpg';
 
 class ActorPage extends Component {
+    componentWillMount() {
+        const {id} = this.props.match.params;
+        this.props.getPeopleProfile(id);
+    }
     componentDidUpdate(prevProps, prevState) {
         console.log('Hello');
         console.log(prevProps);
         if (prevProps.match.params.id !== this.props.match.params.id) {
-
+            this.props.getPeopleProfile(this.props.match.params.id);
         }
     }
     render() {
+        const { profile } = this.props;
+        const onErrorLoadingImage = (e) => {
+            e.target.src = placeholderImage;
+        };
+        const imageStyle = {
+            display: 'block',
+            //clipPath: 'polygon(0 0, 100% 0%, 100% 90%, 0 100%)',
+            objectFit: 'cover',
+            objectPosition: 'center'
+        };
         return (
-            <div className="jumbotron">
-                <h1 className="text-center page-title">Top Series {this.props.match.params.id}</h1>
-                <p>This is a template showcasing the optional theme stylesheet included in Bootstrap. Use it as a starting point to create something more unique by building on or modifying it.</p>
+            <div className="row">
+                <div className="col-md-3">
+                    <Card style={{backgroundColor: 'transparent', border: 'none'}}>
+                        <CardImg top width="100%" src={`https://image.tmdb.org/t/p/w276_and_h350_face${profile.profile_path}`}
+                                 alt={profile.name} onError={onErrorLoadingImage} style={imageStyle}/>
+                        <CardBody style={{paddingRight: 0, paddingLeft: 0}}>
+                            <CardTitle className="h6" style={{fontSize: 14, height: 30}}>
+                                <Link to={`/actors/${profile.id}`}>{profile.name}</Link>
+                            </CardTitle>
+                            <CardSubtitle style={{fontSize: 12, paddingTop: 0, marginTop: 5}}>
+                                <strong>Birth day:</strong> {profile.birthday}
+                            </CardSubtitle>
+                            <CardSubtitle style={{fontSize: 12, paddingTop: 0, marginTop: 5}}>
+                                <strong>Place of birth:</strong> {profile.place_of_birth}
+                            </CardSubtitle>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div className="col-md-9">
+                    <p>
+                        {profile.biography}
+                    </p>
+                </div>
             </div>
         )
     }
 }
 
-export default ActorPage;
+function mapStateToProps(state) {
+    return state.actorStore;
+}
+
+export default connect(mapStateToProps, { getPeopleProfile })(ActorPage);

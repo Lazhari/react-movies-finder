fde3e76f758b74b0abe30245e09c72f9f4c1b7dc
Lazhari <lazhari.mohammed@outlook.com>
2018-02-18 16:16:08 +0000

    Improve the movie card, header and sidebar ui

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
-import {Card, CardBody, CardTitle, CardImg} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import { Card, CardBody, CardTitle, CardImg } from 'reactstrap';
 
 import './styles/MovieCard.css';
 import placeholderImage from '../Images/abstract-image.jpg';
 
-const MovieCard = ({movie, loading, hideOverview}) => {
+const MovieCard = ({ movie, loading, hideOverview }) => {
     const getYear = (stringDate) => {
         const d = new Date(stringDate);
         return d.getFullYear();
@@ -15,15 +15,15 @@ const MovieCard = ({movie, loading, hideOverview}) => {
     };
     const imageStyle = {
         display: 'block',
-        height: '40vh',
+        height: '50vh',
         objectFit: 'cover',
         borderRadius: 5.5,
         objectPosition: 'center'
     };
     return (
-        <Card style={{backgroundColor: 'rgba(0,0,0,0)', border: 'none'}}>
+        <Card style={{ backgroundColor: 'rgba(0,0,0,0)', border: 'none' }}>
             <CardImg top width="100%" src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}`}
-                     alt={movie.original_title} onError={onErrorLoadingImage} style={imageStyle}>
+                alt={movie.original_title} onError={onErrorLoadingImage} style={imageStyle}>
             </CardImg>
             <h1 className="circle">
                 {movie.vote_average.toString().split('.')[0]}
@@ -33,14 +33,14 @@ const MovieCard = ({movie, loading, hideOverview}) => {
                     fontWeight: 100
                 }}>.{movie.vote_average.toString().split('.')[1] || 0}</sup>
             </h1>
-            <CardBody style={{padding: 10}}>
+            <CardBody style={{ padding: 10 }}>
                 <CardTitle className=""
-                           style={{height: 34, color: '#e9e9e9', fontWeight: 'bold', textAlign: 'left', fontSize: 16}}>
-                    <NavLink to={`/movies/${movie.id}`} style={{textDecoration: 'none'}}>
+                    style={{ height: 34, color: '#e9e9e9', fontWeight: 'bold', textAlign: 'left', fontSize: 16 }}>
+                    <NavLink to={`/movies/${movie.id}`} style={{ textDecoration: 'none' }}>
                         {movie.original_title}
                     </NavLink>
                 </CardTitle>
-                <span style={{color: '#ffffff', opacity: 0.8, fontSize: 14}}>
+                <span style={{ color: '#ffffff', opacity: 0.8, fontSize: 14 }}>
                     {getYear(movie.release_date) || 'Unknown Released Date'}
                 </span>
             </CardBody>

a9835dde6368118cbe76ab4a5346205551b9b4aa
Lazhari <lazhari.mohammed@outlook.com>
2018-02-26 21:38:03 +0000

    Improve the movie header

--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -23,73 +23,64 @@ class MovieHeader extends Component {
     render() {
         const { movie, genres, productionCompanies, trailer, actors } = this.props;
         const headerStyle = {
-            backgroundImage: `linear-gradient(rgba(3, 3, 3, 0.30), rgba(0, 0, 5, 0.30)), url(https://image.tmdb.org/t/p/w1400_and_h450_bestv2${movie.backdrop_path})`,
-            backgroundSize: 'contain',
-            backgroundPosition: 'center center',
-            lineHeight: '320px',
-            height: 320,
-            borderRadius: 10,
-            color: '#fff',
-            marginBottom: 20
-            //clipPath: 'polygon(1% 0, 100% 0, 99% 100%, 0% 100%)'
-        };
-        const headerImage = {
-            borderRadius: 5,
-            width: '100%'
+            backgroundImage: `linear-gradient(rgba(3, 3, 3, 0.30), rgba(0, 0, 5, 0.30)), url(https://image.tmdb.org/t/p/w1400_and_h450_bestv2${movie.backdrop_path})`
         };
         const getYear = (stringDate) => {
             const d = new Date(stringDate);
             return d.getFullYear();
         };
+        // Trailer video modal
+        const modal = (
+            <Modal isOpen={true} size="lg" isOpen={this.state.modal} toggle={this.toggle} style={{ maxWidth: '100%' }}>
+                <ModalHeader toggle={this.toggle}>{trailer.name}</ModalHeader>
+                <ModalBody>
+                    <ReactPlayer url={`https://www.youtube.com/watch?v=${trailer.key}`} playing height="90vh" width="100%" />
+                </ModalBody>
+            </Modal>
+        );
         return (
             <div className="row">
-                <div className="col-md-12 text-center" style={headerStyle}>
-                    <img src={playIcon} onClick={this.toggle} />
+                <div className="col-md-12 text-center movie-trailer" style={headerStyle}>
+                    <img src={playIcon} onClick={this.toggle} className="movie-trailer__play-icon" />
                     {
-                        trailer.key ? <Modal isOpen={true} size="lg" isOpen={this.state.modal} toggle={this.toggle} style={{ maxWidth: '100%' }}>
-                            <ModalHeader toggle={this.toggle}>{trailer.name}</ModalHeader>
-                            <ModalBody>
-                                <ReactPlayer url={`https://www.youtube.com/watch?v=${trailer.key}`} playing height="90vh" width="100%" />
-                            </ModalBody>
-                        </Modal> : null
+                        trailer.key ? modal : null
                     }
                 </div>
                 <div className="col-md-3">
-                    <img src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}`} className="img-responsive" style={headerImage} />
+                    <img src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}`} className="img-responsive movie-poster" />
                 </div>
                 <div className="col-md-8">
                     <div className="row">
                         <div className="col-md-12">
-                            <h1 style={{ fontSize: 18, color: '#e9e9e9' }}>
+                            <h1 className="movie-title">
                                 {movie.original_title}
                             </h1>
-                            <h4 style={{ fontSize: 18, color: '#e9e9e9' }}>
+                            <h4 className="movie-release">
                                 {getYear(movie.release_date)}
                             </h4>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-12">
-                            <p style={{ color: '#c53364' }}>
+                            <p className="movie-vote">
                                 {movie.vote_average} {' '}
                                 <Rating initialRating={movie.vote_average} emptySymbol="fa fa-star-o"
                                     fullSymbol="fa fa-star" stop={10} step={2} />
-
                                 <Labels labels={genres} />
                             </p>
                         </div>
                     </div>
-                    <div className="row">
+                    <div className="row movie-overview">
                         <div className="col-md-12">
-                            <h6 style={{ fontSize: 18, color: '#e9e9e9' }}>Overview</h6>
-                            <p style={{ color: '#e9e9e9', fontWeight: 100, fontSize: 14 }}>
+                            <h1>Overview</h1>
+                            <p>
                                 {movie.overview}
                             </p>
                         </div>
                     </div>
-                    <div className="row">
+                    <div className="row movie-billed">
                         <div className="col-md-12">
-                            <h1 style={{ fontSize: 18, color: '#e9e9e9' }}>Top Billed Cast</h1>
+                            <h1>Top Billed Cast</h1>
                             <ActorsList actors={actors} />
                         </div>
                     </div>

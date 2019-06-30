78fdd8430684bb30ec14032a71318b31a531ec7c
Lazhari <lazhari.mohammed@outlook.com>
2019-06-30 21:09:40 +0100

    Set up testing with enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { mount } from "enzyme";
+import { MemoryRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import ScrollToTop from "./components/ScrollToTop";
 import App from "./App";
+import store from "./store";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+import HomePage from "./containers/HomePage";
+
+describe("Testing the / route", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(
+      <Router basename="/">
+        <Provider store={store}>
+          <ScrollToTop>
+            <MemoryRouter initialEntries={["/"]}>
+              <App />
+            </MemoryRouter>
+          </ScrollToTop>
+        </Provider>
+      </Router>
+    );
+  });
+
+  it("shows a HomePage", () => {
+    expect(wrapper.find(HomePage).length).toEqual(1);
+  });
+  afterEach(() => {
+    wrapper.unmount();
+  });
 });

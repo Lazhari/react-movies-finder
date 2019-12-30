9632a0113395777d1a551160dc94c703ad065571
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2019-12-30 22:35:26 +0100

    Add the drawer

--- src/App.js
+++ src/App.js
@@ -15,35 +15,139 @@ import ActorPage from "./containers/ActorPage";
 import theme from "./theme";
 import { ThemeProvider } from "@material-ui/core";
 
-class App extends Component {
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Header />
-        <div className="container-fluid">
-          <div className="row">
-            <div
-              className="col-6 col-lg-2 col-md-2 sidebar-offcanvas"
-              id="sidebar"
-            >
-              <MoviesCategoryContainer />
-            </div>
-            <div className="col-12 col-md-10 col-lg-10">
-              <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route exact path="/upcoming" component={UpcomingPage} />
-                <Route exact path="/series" component={TopSeriesPage} />
-                <Route exact path="/genres/:id/:genre" component={GenrePage} />
-                <Route exact path="/movies/:id" component={MoviePage} />
-                <Route exact path="/actors/:id" component={ActorPage} />
-              </Switch>
-            </div>
-          </div>
-        </div>
-      </ThemeProvider>
-    );
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import clsx from "clsx";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  hide: {
+    display: "none"
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: "space-between"
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    marginLeft: 0
   }
+}));
+
+function App() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header handleDrawerOpen={handleDrawerOpen} open={open} />
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <Typography variant="h5" component="h1">
+            Categories
+          </Typography>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <MoviesCategoryContainer handleDrawerClose={handleDrawerClose} />
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open
+        })}
+      >
+        <div className={classes.drawerHeader}>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/upcoming" component={UpcomingPage} />
+            <Route exact path="/series" component={TopSeriesPage} />
+            <Route exact path="/genres/:id/:genre" component={GenrePage} />
+            <Route exact path="/movies/:id" component={MoviePage} />
+            <Route exact path="/actors/:id" component={ActorPage} />
+          </Switch>
+        </div>
+      </main>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/components/blocks/Header.js
+++ src/components/blocks/Header.js
@@ -9,18 +9,38 @@ import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
 import Link from "@material-ui/core/Link";
+import clsx from "clsx";
 
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
 
 import logo from "../../logo.svg";
 
+const drawerWidth = 240;
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
   },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
   menuButton: {
     marginRight: theme.spacing(2)
+  },
+  hide: {
+    display: "none"
   },
   title: {
     flexGrow: 1
@@ -73,18 +93,24 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-function Header() {
+function Header({ handleDrawerOpen, open }) {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <AppBar position="fixed">
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open
+        })}
+      >
         <LoadingBar className={classes.loadingBar} />
         <Toolbar>
           <IconButton
             edge="start"
-            className={classes.menuButton}
+            className={clsx(classes.menuButton, open && classes.hide)}
             color="inherit"
             aria-label="menu"
+            onClick={handleDrawerOpen}
           >
             <MenuIcon />
           </IconButton>

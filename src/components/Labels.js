6dbe6bfd66afe799fa26940b7a36bfb92db074f4
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2020-01-01 00:10:55 +0100

    Rewrite the movie labels component

--- src/components/Labels.js
+++ src/components/Labels.js
@@ -1,13 +1,28 @@
 import React from "react";
-import { Badge } from "reactstrap";
+import { makeStyles } from "@material-ui/core/styles";
+import Chip from "@material-ui/core/Chip";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(0.5)
+    }
+  }
+}));
 
 const Labels = ({ labels }) => {
+  const classes = useStyles();
   return (
-    <span style={{ paddingLeft: 20 }}>
+    <span className={classes.root}>
       {labels.map(label => (
-        <Badge color="primary" key={label.id} style={{ marginRight: 5 }}>
-          {label.name}
-        </Badge>
+        <Chip
+          variant="outlined"
+          size="small"
+          label={label.name}
+          key={label.id}
+        />
       ))}
     </span>
   );

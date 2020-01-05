c1de6daf24bec5f9fc021ff90c2b86756e1936cf
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2020-01-05 22:44:57 +0100

    Re-write the Top series page component

--- src/containers/TopSeriesPage.js
+++ src/containers/TopSeriesPage.js
@@ -1,36 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 
-import { withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 
-const styles = {
+const useStyles = makeStyles(theme => ({
   root: {
     marginTop: 50
   }
+}));
+
+const TopSeriesPage = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Card>
+        <CardContent>
+          <Typography variant="h4" component="h1">
+            Top Series
+          </Typography>
+          <Typography variant="body1">
+            This is a template showcasing the optional theme stylesheet included
+            in Bootstrap. Use it as a starting point to create something more
+            unique by building on or modifying it.
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
-
-class TopSeriesPage extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        <Card>
-          <CardContent>
-            <Typography variant="h4" component="h1">
-              Top Series
-            </Typography>
-            <Typography variant="body1">
-              This is a template showcasing the optional theme stylesheet
-              included in Bootstrap. Use it as a starting point to create
-              something more unique by building on or modifying it.
-            </Typography>
-          </CardContent>
-        </Card>
-      </div>
-    );
-  }
-}
-
-export default withStyles(styles)(TopSeriesPage);
+export default TopSeriesPage;

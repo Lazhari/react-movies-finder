c764217a824af81246cebe6761668eccc671a0ad
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2020-01-06 22:25:48 +0100

    Fixed the redux promise middleware issue

--- src/store.js
+++ src/store.js
@@ -1,13 +1,15 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
-import promise from "redux-promise-middleware";
+import { createPromise } from "redux-promise-middleware";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { loadingBarMiddleware } from "react-redux-loading-bar";
 
 import rootReducer from "./reducers";
 
+const promise = createPromise({ types: { fulfilled: "success" } });
+
 const middleware = composeWithDevTools(
-  applyMiddleware(promise(), thunk, loadingBarMiddleware())
+  applyMiddleware(promise, thunk, loadingBarMiddleware())
 );
 
 export default createStore(rootReducer, middleware);

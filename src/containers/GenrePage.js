55c81ba70051d657396b249af21b74f2b82770e3
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2020-01-07 20:07:20 +0100

    Rewrite the Genre page

--- src/containers/GenrePage.js
+++ src/containers/GenrePage.js
@@ -1,68 +1,65 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Pagination from "material-ui-flat-pagination";
-import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { fetchMoviesByGenre } from "../actions/moviesActions";
 
 import MoviesCardList from "../components/MoviesCardList";
+import Loader from "../components/common/Loader";
 
-const styles = {
+const useStyles = makeStyles(theme => ({
   root: {
     marginTop: 50
   },
   paginationContainer: {
     display: "flex",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    marginTop: theme.spacing(2)
   }
+}));
+
+const GenrePage = ({ match }) => {
+  const dispatch = useDispatch();
+  const { movies, loading, page, totalResults } = useSelector(
+    state => state.moviesStore
+  );
+  const classes = useStyles();
+
+  const handlePageChange = offset => {
+    const pageNumber = offset / 20 + 1;
+    dispatch(fetchMoviesByGenre(pageNumber, match.params.id));
+    window.scrollTo(0, 0);
+  };
+
+  useEffect(() => {
+    dispatch(fetchMoviesByGenre(1, match.params.id));
+  }, [dispatch, match.params.id]);
+
+  return (
+    <div className={classes.root}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        {match.params.genre}
+      </Typography>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <MoviesCardList movies={movies} cols={6} />
+          <div className={classes.paginationContainer}>
+            <Pagination
+              limit={20}
+              offset={(page - 1) * 20}
+              total={totalResults}
+              onClick={(e, offset) => handlePageChange(offset)}
+            />
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
-class GenrePage extends Component {
-  componentWillMount() {
-    this.props.fetchMoviesByGenre(1, this.props.match.params.id);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.match.params.id !== this.props.match.params.id) {
-      this.props.fetchMoviesByGenre(1, this.props.match.params.id);
-    }
-  }
-
-  handlePageChange(offset) {
-    const pageNumber = offset / 20 + 1;
-    this.props.fetchMoviesByGenre(pageNumber, this.props.match.params.id);
-    window.scrollTo(0, 0);
-  }
-
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          {this.props.match.params.genre}
-        </Typography>
-        <MoviesCardList movies={this.props.movies} cols={6} />
-        <div className={classes.paginationContainer}>
-          <Pagination
-            limit={20}
-            offset={(this.props.page - 1) * 20}
-            total={this.props.totalResults}
-            onClick={(e, offset) => this.handlePageChange(offset)}
-          />
-        </div>
-      </div>
-    );
-  }
-}
-
-function mapStateToProps(state) {
-  const { movies, loading, page, totalResults } = state.moviesStore;
-  return { movies, loading, page, totalResults };
-}
-
-export default connect(
-  mapStateToProps,
-  { fetchMoviesByGenre }
-)(withStyles(styles)(GenrePage));
+export default GenrePage;

0d7820d8746a6ab0197defe0fe189034fb3c833a
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2020-01-07 20:32:31 +0100

    Rewrite the Movies category container

--- src/containers/MoviesCategoryContainer.js
+++ src/containers/MoviesCategoryContainer.js
@@ -1,33 +1,24 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import Sidebar from "../components/blocks/Sidebar";
 
 import { fetchGenre } from "../actions/moviesActions";
 
-class MoviesCategoryContainer extends Component {
-  componentDidMount() {
-    this.props.fetchGenre();
-  }
+const MoviesCategoryContainer = ({ handleDrawerClose }) => {
+  const dispatch = useDispatch();
+  const { genres, genreId, loading } = useSelector(state => state.moviesStore);
+  useEffect(() => {
+    dispatch(fetchGenre());
+  }, [dispatch]);
+  return (
+    <Sidebar
+      genres={genres}
+      loading={loading}
+      genreId={genreId}
+      handleDrawerClose={handleDrawerClose}
+    />
+  );
+};
 
-  render() {
-    return (
-      <Sidebar
-        genres={this.props.genres}
-        loading={this.props.loading}
-        genreId={this.props.genreId}
-        handleDrawerClose={this.props.handleDrawerClose}
-      />
-    );
-  }
-}
-
-function mapStateToProps(state) {
-  const { genres, genreId, loading } = state.moviesStore;
-  return { genres, genreId, loading };
-}
-
-export default connect(
-  mapStateToProps,
-  { fetchGenre }
-)(MoviesCategoryContainer);
+export default MoviesCategoryContainer;

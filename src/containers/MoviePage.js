ffc3d221f8b254e1d04ab0e4d559d8a4daf4dd2c
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2020-01-08 20:32:11 +0100

    Rewrite the movie page

--- src/containers/MoviePage.js
+++ src/containers/MoviePage.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import { withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 
 import {
@@ -16,99 +16,88 @@ import {
 import MovieHeader from "../components/MovieHeader";
 import MoviesCardList from "../components/MoviesCardList";
 import ReviewsList from "../components/ReviewsList";
+import Loader from "../components/common/Loader";
 
-const styles = {
+const useStyles = makeStyles(theme => ({
   root: {},
   reviewsContainer: {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 16
+    marginTop: theme.spacing(2)
   },
   moviesContainer: {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 16
+    marginTop: theme.spacing(2)
   }
+}));
+
+const MoviePage = ({ match }) => {
+  const dispatch = useDispatch();
+  const {
+    movie,
+    loading,
+    genres,
+    trailer,
+    actors,
+    reviews,
+    relatedMovies,
+    productionCompanies
+  } = useSelector(state => state.movieStore);
+  const classes = useStyles();
+
+  useEffect(() => {
+    dispatch(fetchMovie(match.params.id));
+    dispatch(fetchMovieVideos(match.params.id));
+    dispatch(fetchMovieReviews(match.params.id));
+    dispatch(fetchRelatedMovies(match.params.id));
+    dispatch(fetchMovieActors(match.params.id));
+    return () => {
+      cleaningMovieReducer();
+    };
+  }, [dispatch, match.params.id]);
+
+  return (
+    <div className={classes.root}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <MovieHeader
+            movie={movie}
+            genres={genres}
+            productionCompanies={productionCompanies}
+            trailer={trailer}
+            actors={actors}
+          />
+          {reviews && reviews.length && (
+            <div className={classes.reviewsContainer}>
+              <Typography variant="h5" component="h1" gutterBottom>
+                Reviews
+              </Typography>
+              <ReviewsList reviews={reviews} />
+            </div>
+          )}
+          {relatedMovies && relatedMovies.length && (
+            <div className={classes.moviesContainer}>
+              <Typography variant="h5" component="h1" gutterBottom>
+                Related Movies
+              </Typography>
+              <MoviesCardList
+                movies={relatedMovies}
+                hideOverview={true}
+                itemsPerRow={4}
+              />
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
 };
 
-class MoviePage extends Component {
-  componentWillMount() {
-    const { id } = this.props.match.params;
-    this.props.fetchMovie(id);
-    this.props.fetchMovieVideos(id);
-    this.props.fetchMovieReviews(id);
-    this.props.fetchRelatedMovies(id);
-    this.props.fetchMovieActors(id);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.match.params.id !== this.props.match.params.id) {
-      this.props.cleaningMovieReducer();
-      const { id } = this.props.match.params;
-      this.props.fetchMovie(id);
-      this.props.fetchMovieVideos(id);
-      this.props.fetchMovieReviews(id);
-      this.props.fetchRelatedMovies(id);
-      this.props.fetchMovieActors(id);
-    }
-  }
-
-  componentWillUnmount() {
-    this.props.cleaningMovieReducer();
-  }
-
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        <MovieHeader
-          movie={this.props.movie}
-          genres={this.props.genres}
-          productionCompanies={this.props.productionCompanies}
-          trailer={this.props.trailer}
-          actors={this.props.actors}
-        />
-        {this.props.reviews && this.props.reviews.length && (
-          <div className={classes.reviewsContainer}>
-            <Typography variant="h5" component="h1" gutterBottom>
-              Reviews
-            </Typography>
-            <ReviewsList reviews={this.props.reviews} />
-          </div>
-        )}
-        {this.props.relatedMovies && this.props.relatedMovies.length && (
-          <div className={classes.moviesContainer}>
-            <Typography variant="h5" component="h1" gutterBottom>
-              Related Movies
-            </Typography>
-            <MoviesCardList
-              movies={this.props.relatedMovies}
-              hideOverview={true}
-              itemsPerRow={4}
-            />
-          </div>
-        )}
-      </div>
-    );
-  }
-}
-
-function mapStateToProps(state) {
-  return state.movieStore;
-}
-
-export default connect(
-  mapStateToProps,
-  {
-    fetchMovie,
-    fetchMovieVideos,
-    fetchMovieReviews,
-    fetchRelatedMovies,
-    fetchMovieActors,
-    cleaningMovieReducer
-  }
-)(withStyles(styles)(MoviePage));
+export default MoviePage;

7fe0575a5002bad074238837da7862b16d2ce5f6
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2020-01-13 10:50:17 +0100

    Add the genre filter to the top series page

--- src/containers/TopSeriesPage.js
+++ src/containers/TopSeriesPage.js
@@ -5,10 +5,14 @@ import Pagination from "material-ui-flat-pagination";
 
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
+import ListItemText from "@material-ui/core/ListItemText";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
+import Input from "@material-ui/core/Input";
+import Chip from "@material-ui/core/Chip";
+import Checkbox from "@material-ui/core/Checkbox";
 
-import { fetchTvShows } from "../actions/tvShowsActions";
+import { fetchTvShows, fetchTvGenres } from "../actions/tvShowsActions";
 import TvShowList from "../components/TvShowList";
 import Loader from "../components/common/Loader";
 
@@ -24,12 +28,19 @@ const useStyles = makeStyles(theme => ({
   },
   filtersContainer: {
     display: "flex",
-    justifyContent: "flex-end",
+    justifyContent: "space-between",
     marginBottom: theme.spacing(2)
   },
   formControl: {
     margin: theme.spacing(1),
     minWidth: 120
+  },
+  chips: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  chip: {
+    margin: 2
   }
 }));
 
@@ -62,15 +73,21 @@ const sortOptions = [
 
 const TopSeriesPage = () => {
   const dispatch = useDispatch();
-  const { tvShows, loading, page, totalResults } = useSelector(
+  const { tvShows, loading, page, totalResults, genres } = useSelector(
     state => state.tvShows
   );
   const classes = useStyles();
   const [sortBy, setSortBy] = React.useState("popularity.desc");
+  const [selectedGenres, setSelectedGenres] = React.useState([]);
 
   const handlePageChange = offset => {
     const pageNumber = offset / 20 + 1;
-    dispatch(fetchTvShows(pageNumber, { sort_by: sortBy }));
+    dispatch(
+      fetchTvShows(pageNumber, {
+        sort_by: sortBy,
+        with_genres: selectedGenres.join(",")
+      })
+    );
     window.scrollTo(0, 0);
   };
 
@@ -78,13 +95,68 @@ const TopSeriesPage = () => {
     setSortBy(event.target.value);
   };
 
+  const handleGenresChange = event => {
+    setSelectedGenres(event.target.value);
+  };
+
   useEffect(() => {
-    dispatch(fetchTvShows(1, { sort_by: sortBy }));
-  }, [dispatch, sortBy]);
+    dispatch(
+      fetchTvShows(1, {
+        sort_by: sortBy,
+        with_genres: selectedGenres.join(",")
+      })
+    );
+  }, [dispatch, sortBy, selectedGenres]);
+  useEffect(() => {
+    dispatch(fetchTvGenres());
+  }, [dispatch]);
+
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250
+      }
+    }
+  };
 
   return (
     <div className={classes.root}>
       <div className={classes.filtersContainer}>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-mutiple-chip-label">Genre</InputLabel>
+          <Select
+            labelId="demo-mutiple-chip-label"
+            id="demo-mutiple-chip"
+            multiple
+            value={selectedGenres}
+            onChange={handleGenresChange}
+            input={<Input id="select-multiple-chip" />}
+            renderValue={selected => (
+              <div className={classes.chips}>
+                {genres
+                  .filter(genre => selected.includes(genre.id))
+                  .map(genre => (
+                    <Chip
+                      key={genre.id}
+                      label={genre.name}
+                      className={classes.chip}
+                    />
+                  ))}
+              </div>
+            )}
+            MenuProps={MenuProps}
+          >
+            {genres.map(genre => (
+              <MenuItem key={genre.id} value={genre.id}>
+                <Checkbox checked={selectedGenres.indexOf(genre.id) > -1} />
+                <ListItemText primary={genre.name} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
         <FormControl className={classes.formControl}>
           <InputLabel id="demo-simple-select-label">Sorted By</InputLabel>
           <Select

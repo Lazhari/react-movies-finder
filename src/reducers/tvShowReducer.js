cc88790c555e58c45d800135cdd080c0bf015cb9
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2020-01-20 18:59:27 +0100

    Updated the tv show reducer

--- src/reducers/tvShowReducer.js
+++ src/reducers/tvShowReducer.js
@@ -1,4 +1,4 @@
-import {} from "../actions/actionsType";
+import { FETCH_TV_SHOW_DETAILS } from "../actions/actionsType";
 
 const defaultState = {
   loading: false,
@@ -7,6 +7,19 @@ const defaultState = {
 
 export default (state = defaultState, action = {}) => {
   switch (action.type) {
+    case `${FETCH_TV_SHOW_DETAILS}_PENDING`: {
+      return {
+        ...state,
+        loading: true
+      };
+    }
+    case `${FETCH_TV_SHOW_DETAILS}_FULFILLED`: {
+      return {
+        ...state,
+        loading: false,
+        tvShow: action.payload.data
+      };
+    }
     default:
       return state;
   }

4a44e4a547f19eb11210b003d70a0213b41d095a
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2021-02-09 16:48:37 +0100

    test: set up the test and implement test for home and upcoming pages

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
   },
   watchPlugins: [
     'jest-watch-typeahead/filename',

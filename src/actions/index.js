7c32bea950965d71ef30e082259030c0eb167e08
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2021-02-11 00:38:08 +0100

    fix: update the http client config

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,22 +2,8 @@ import axios from 'axios'
 
 export const client = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
-})
-
-client.interceptors.request.use(
-  function (config) {
-    // Do something before request is sent
-
-    return {
-      ...config,
-      params: {
-        api_key: 'fed69657ba4cc6e1078d2a6a95f51c8c',
-        language: 'en-US',
-      },
-    }
+  params: {
+    api_key: 'fed69657ba4cc6e1078d2a6a95f51c8c',
+    language: 'en-US',
   },
-  function (error) {
-    // Do something with request error
-    return Promise.reject(error)
-  }
-)
+})

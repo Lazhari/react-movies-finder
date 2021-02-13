8c2c048caea842f1ecff27f8c76ee02ee52a272e
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2021-02-13 01:16:54 +0100

    test: fix the jest config

--- jest.config.js
+++ jest.config.js
@@ -13,5 +13,10 @@ module.exports = {
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+    '^@src(.*)$': ['<rootDir>/src$1'],
+    '^@components(.*)$': ['<rootDir>/src/components$1'],
+    '^@actions(.*)$': ['<rootDir>/src/actions$1'],
+    '^@models(.*)$': ['<rootDir>/src/models$1'],
+    '^@reducers(.*)$': ['<rootDir>/src/reducers$1'],
   },
 }

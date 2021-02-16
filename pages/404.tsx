053f083f8739e66b8de5dcfbf0a48ffa855a384d
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2021-02-17 00:45:44 +0100

    feat: :sparkles: create the custom not found page

---
+++ pages/404.tsx
@@ -0,0 +1,56 @@
+import { NextPage } from 'next'
+import Link from 'next/link'
+
+import { makeStyles, Theme } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+
+import NotFoundIcon from '@components/common/icons/NotFound'
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    marginTop: theme.spacing(10),
+    flexDirection: 'column',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    marginTop: theme.spacing(2),
+  },
+  homeButton: {
+    marginTop: theme.spacing(2),
+  },
+}))
+
+const Custom404: NextPage = () => {
+  const classes = useStyles()
+  return (
+    <Container className={classes.root} maxWidth="xl">
+      <div>
+        <NotFoundIcon color="inherit" style={{ fontSize: 160 }} />
+      </div>
+      <Typography
+        align="center"
+        variant="h5"
+        component="h1"
+        className={classes.title}
+        gutterBottom
+      >
+        Page Not Found
+      </Typography>
+      <Typography align="center" variant="body1">
+        Oops! We can&apos;t find the page you are looking for!!
+      </Typography>
+
+      <Link href="/">
+        <Button variant="outlined" className={classes.homeButton}>
+          Back to Home
+        </Button>
+      </Link>
+    </Container>
+  )
+}
+
+export default Custom404
---
+++ src/components/common/icons/NotFound.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import SvgIcon from '@material-ui/core/SvgIcon'
+
+function NotFoundIcon(props) {
+  return (
+    <SvgIcon {...props} viewBox="0 0, 60 60">
+      <path d="M57 0H3C1.346 0 0 1.346 0 3v56a1 1 0 001 1h58a1 1 0 001-1V3c0-1.654-1.346-3-3-3zM3 2h54c.551 0 1 .449 1 1v5H2V3c0-.551.449-1 1-1zm16 51v5h-8v-5h8zm2 0h8v5h-8v-5zm10 0h8v5h-8v-5zm10 0h8v5h-8v-5zM2 51V17h56v34H2zm0-36v-5h56v5H2zm0 38h7v5H2v-5zm49 5v-5h7v5h-7z" />
+      <path d="M5 4h4v2H5zM12 4h4v2h-4zM16 24h-2v13H8V24H6v14a1 1 0 001 1h7v6h2v-6h3v-2h-3V24zM54 37h-3V24h-2v13h-6V24h-2v14a1 1 0 001 1h7v6h2v-6h3v-2zM30 24c-3.86 0-7 3.14-7 7v7c0 3.86 3.14 7 7 7s7-3.14 7-7v-7c0-3.86-3.14-7-7-7zm5 14c0 2.757-2.243 5-5 5s-5-2.243-5-5v-7c0-2.757 2.243-5 5-5s5 2.243 5 5v7z" />
+    </SvgIcon>
+  )
+}
+
+export default NotFoundIcon

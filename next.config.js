d98b9def30c7f0d4a8706c9e3fc034ed257fa675
Mohammed Lazhari <lazhari.mohammed@gmail.com>
2024-08-29 15:20:55 +0100

    chore: Delete the old Nextjs dependencies and setup Shadcn ui

--- next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  images: {
-    domains: ['image.tmdb.org'],
-  },
-}
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['image.tmdb.org'],
+  },
+}
+
+export default nextConfig
